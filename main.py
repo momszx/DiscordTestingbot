67c38571fbc127da9af442f12cf0be9fd23262e4
Donkó Márk <55428772+momszx@users.noreply.github.com>
2023-10-06 22:18:07 +0200

    Add some upgrade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import discord
-import os
 from discord.ext import commands
 
 class BOT:
@@ -8,16 +7,17 @@ class BOT:
         self.intents.message_content = True
         self.intents.typing = False
         self.intents.presences = False
-        self.guild = ""
+        self.guild = None
         self.bot = commands.Bot(command_prefix='!', intents=self.intents)
 
-        @self.bot.tree.command(name="commandname", description="My first application Command", guild=discord.Object(id=env.GUILD_ID))  # Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+        @self.bot.tree.command(name="commandname", description="My first application Command", guild=discord.Object(
+            id=9))
         async def first_command(interaction):
             await interaction.response.send_message("Hello!")
+
         @self.bot.event
         async def on_ready():
-            guild_id = env.GUILD_ID
-            print(type(guild_id))
+            guild_id = 9
             self.guild = self.bot.get_guild(guild_id)
             if self.guild is not None:
                 await self.bot.tree.sync(guild=discord.Object(id=guild_id))
@@ -25,6 +25,5 @@ class BOT:
             else:
                 print("Nem található a szerver.")
 
-        self.bot.run('env.DC_TOKEN')
-
-BOT = BOT()
+if __name__ == "__main__":
+    BOT().bot.run("a")
